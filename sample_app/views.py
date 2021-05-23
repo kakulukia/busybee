b5f084c6a5440da7342794db6892b1235b604c88
Andy <andy@freilandikiws.de>
2021-05-23 19:50:15 +0200

    inkl World

--- sample_app/views.py
+++ sample_app/views.py
@@ -3,3 +3,11 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'pages/index.html'
+
+
+class ContactView(TemplateView):
+    template_name = 'pages/kontakt.html'
+
+
+class WorldView(TemplateView):
+    template_name = 'pages/world.html'
--- settings/urls.py
+++ settings/urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from sample_app.views import IndexView
+from sample_app.views import IndexView, ContactView, WorldView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', IndexView.as_view()),
+    url(r'kontakt$', ContactView.as_view()),
+    url(r'busy-bee-world$', WorldView.as_view()),
 ]
 
 if settings.DEBUG:
