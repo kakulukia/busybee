2efe6044cfe75f7a765b95d4f17f27500eff24da
Andy <andy@freilandikiws.de>
2021-05-24 08:35:19 +0200

    alle Seiten sind mit dabei

--- sample_app/views.py
+++ sample_app/views.py
@@ -2,28 +2,60 @@ from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
-    template_name = 'pages/index.html'
+    template_name = "pages/index.html"
 
 
 class ContactView(TemplateView):
-    template_name = 'pages/kontakt.html'
+    template_name = "pages/kontakt.html"
 
 
 class WorldView(TemplateView):
-    template_name = 'pages/world.html'
+    template_name = "pages/world.html"
 
 
 class RequestView(TemplateView):
-    template_name = 'pages/anfrage.html'
+    template_name = "pages/anfrage.html"
 
 
 class PrivacyView(TemplateView):
-    template_name = 'pages/datenschutz.html'
+    template_name = "pages/datenschutz.html"
 
 
 class ImprintView(TemplateView):
-    template_name = 'pages/impressum.html'
+    template_name = "pages/impressum.html"
 
 
 class TermsView(TemplateView):
-    template_name = 'pages/agb.html'
+    template_name = "pages/agb.html"
+
+
+class BuffetView(TemplateView):
+    template_name = "pages/buffet.html"
+
+
+class FingerView(TemplateView):
+    template_name = "pages/finger.html"
+
+
+class InspireView(TemplateView):
+    template_name = "pages/inspiration.html"
+
+
+class BioView(TemplateView):
+    template_name = "pages/bio.html"
+
+
+class ServiceView(TemplateView):
+    template_name = "pages/service.html"
+
+
+class DrinksView(TemplateView):
+    template_name = "pages/drinks.html"
+
+
+class LunchView(TemplateView):
+    template_name = "pages/lunch.html"
+
+
+class EventsView(TemplateView):
+    template_name = "pages/events.html"
--- settings/urls.py
+++ settings/urls.py
@@ -1,8 +1,11 @@
 from django.conf import settings
-from django.conf.urls import url, include
+from django.conf.urls import include, url
 from django.contrib import admin
 
-from sample_app.views import IndexView, ContactView, WorldView, RequestView, PrivacyView, ImprintView, TermsView
+from sample_app.views import (BioView, BuffetView, ContactView, FingerView,
+                              ImprintView, IndexView, InspireView, PrivacyView,
+                              RequestView, ServiceView, TermsView, WorldView,
+                              DrinksView, LunchView, EventsView)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -13,6 +16,14 @@ urlpatterns = [
     url(r'datenschutz$', PrivacyView.as_view()),
     url(r'impressum', ImprintView.as_view()),
     url(r'agb', TermsView.as_view()),
+    url(r'buffet-menue', BuffetView.as_view()),
+    url(r'fingerfood', FingerView.as_view()),
+    url(r'inspiration', InspireView.as_view()),
+    url(r'bio-und-allergene-welt', BioView.as_view()),
+    url(r'full-service', ServiceView.as_view()),
+    url(r'getraenke', DrinksView.as_view()),
+    url(r'office-lunch', LunchView.as_view()),
+    url(r'event-consulting', EventsView.as_view()),
 ]
 
 if settings.DEBUG:
